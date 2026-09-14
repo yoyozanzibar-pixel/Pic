4e510bbd2c8547a24dca51360d53b70d8adae26a
yoyozanzibar-pixel <yoyozanzibar@gmail.com>
2026-09-14 07:44:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from flask import Flask app = Flask(name)
+from flask import Flask
+app = Flask(__name__)
 @app.route("/") def index(): return "Hello from Python on Render!"
-if name == "main": app.run()
+if __name__ == "__main__": app.run()
