e8066ddaa6288a0f3a23b3fa25b61a203f8816c4
yoyozanzibar-pixel <yoyozanzibar@gmail.com>
2026-09-14 08:02:01 +0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-  return "Hello from Python on Render!"
-  
 if __name__ == "__main__": app.run()
+
+import logging from aiogram import Bot, Dispatcher, types from aiogram.filters import Command
+TOKEN = "8840599388:AAFbCBD6KmfzTs_XvXfdfhmMMwfDxp7SXYo" logging.basicConfig(level=logging.INFO) bot = Bot(token=8840599388:AAFbCBD6KmfzTs_XvXfdfhmMMwfDxp7SXYo) dp = Dispatcher()
+@dp.message(Command("start")) async def cmd_start(message: types.Message): await message.answer("Привет! Отправь мне текст, фото или видео, и я сохраню их.")
+@dp.message() async def save_content(message: types.Message): if message.text: await message.answer(f"Текст сохранен: {message.text}") elif message.photo: await message.answer("Фото сохранено.") elif message.video: await message.answer("Видео сохранено.")
+async def main(): await dp.start_polling(bot)
+if name == "main": import asyncio asyncio.run(main())
